6224c9fc59abb1d67f70137e1aceb7b7f1187647
Uri Goldshtein <uri.goldshtein@gmail.com>
2014-10-29 20:26:04 +0200

    First working version

--- package.js
+++ package.js
@@ -1,14 +1,18 @@
 Package.describe({
   name: 'urigo:angular-utils-pagination',
   summary: 'Magical automatic pagination for anything in AngularJS',
-  version: '1.0.0',
+  version: '1.0.2',
   git: 'https://github.com/Urigo/meteor-angular-utils-pagination.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
 
-  api.addFiles(['dirPagination.js', 'dirPagination.tpl.html'],'client');
+  api.use('urigo:angular@0.5.1', 'client');
+  api.use('urigo:angular-ui-router@0.6.1', 'client');
+  api.use(['templating'], 'client');
+
+  api.addFiles(['dirPagination.tpl.html', 'dirPagination.js'],'client');
 });
 
 Package.onTest(function(api) {
